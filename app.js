66fd60fcdbbf25bcf36dc0354515ccdc92331ce3
BaraDroid <lambeinovab@seznam.cz>
2025-09-19 12:27:18 +0200

    game structure enriched with sound effects

---
+++ app.js
@@ -0,0 +1,20 @@
+const app = new PIXI.Application();
+
+document.body.appendChild(app.view);
+
+const rocket = PIXI.Sprite.from("assets/rocket.png");
+rocket.x = 350;
+rocket.y = 520;
+rocket.scale.x = 0.05;
+rocket.scale.y = 0.05;
+app.stage.addChild(rocket);
+
+gameInterval(function () {
+  const ufo = PIXI.Sprite.from("assets/ufo1.png");
+  ufo.x = 350;
+  ufo.y = 10;
+  ufo.scale.x = 0.1;
+  ufo.scale.y = 0.1;
+  app.stage.addChild(ufo);
+  flyDown(ufo, 1);
+}, 1000);
